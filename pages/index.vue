024fbe8483d833367ffffbc7e073c9b6d6a810dc
CricaDev <cricadev@Cristians-MacBook-Pro.local>
2022-08-05 22:59:39 -0500

    new colors

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,8 @@
   <div class="text-4xl text-center bg-black">
     <h1 class="m-10 text-white text-xl4_d">CRICA HOME</h1>
     <a href="/" class="link">HOME</a>
+    <NuxtLink to="/portfolio" class="link">PORTFOLIO</NuxtLink>
+
     <a href="/portfolio/" class="link">PORTFOLIO</a>
     <a href="/about/" class="link">ABOUT ME</a>
     <a href="/blog/" class="link">BLOG</a>
