a7b5dcd650dca3fddddf53e52d22c15f45659757
CricaDev <cricadev@Cristians-MacBook-Pro.local>
2022-08-05 20:04:11 -0500

    new routing

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,16 +1,19 @@
 <template lang="">
   <div class="text-4xl text-center">
     <h1 class="m-10 text-9xl">CRICABLOG</h1>
-    <a href="/blog/how-to-learn-fast" class="p-4 text-green-500 underline"
-      >blog 1</a
-    >
-    <a href="/blog/welcome-to-cricadev" class="p-4 text-green-500 underline"
-      >blog 2</a
-    >
+
     <a href="/" class="p-4 text-green-500 underline">HOME</a>
     <a href="/portfolio/" class="p-4 text-green-500 underline">PORTFOLIO</a>
     <a href="/about/" class="p-4 text-green-500 underline">ABOUT ME</a>
-    <a href="/blog/">BLOG</a>
+    <a href="/blog/" class="p-4 text-green-500 underline">BLOG</a>
+    <div class="block">
+      <a href="/blog/how-to-learn-fast" class="p-4 text-green-500 underline"
+        >blog 1</a
+      >
+      <a href="/blog/welcome-to-cricadev" class="p-4 text-green-500 underline"
+        >blog 2</a
+      >
+    </div>
   </div>
 </template>
 <script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,12 @@
-<template></template>
+<template>
+  <div class="text-4xl text-center">
+    <h1 class="m-10 text-9xl">CRICA HOME</h1>
+    <a href="/" class="p-4 text-green-500 underline">HOME</a>
+    <a href="/portfolio/" class="p-4 text-green-500 underline">PORTFOLIO</a>
+    <a href="/about/" class="p-4 text-green-500 underline">ABOUT ME</a>
+    <a href="/blog/" class="p-4 text-green-500 underline">BLOG</a>
+  </div>
+</template>
 
 <script>
 export default {};
