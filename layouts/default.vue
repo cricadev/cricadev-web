eb2626a8676b24e0ea1485e3f848bb1d074d1e91
CricaDev <cricadev@Cristians-MacBook-Pro.local>
2022-08-06 14:44:47 -0500

    new layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,9 +7,11 @@
 </template>
 <script>
 import Header from "@/components/header.vue";
+import Footer from "@/components/footer.vue";
 export default {
   components: {
     Header,
+    Footer,
   },
 };
 </script>
